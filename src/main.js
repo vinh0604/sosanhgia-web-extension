a2ed398146129976782366d163b5e48612f00368
vinhbachsy <bsdvinhuit@gmail.com>
2016-02-06 01:42:43 +0700

    Change DOMSubtreeModified event to MutationObserver

--- src/main.js
+++ src/main.js
@@ -13,31 +13,34 @@ const POTENTIAL_VENDORS = [
 const DOMAIN_PREFIX = /^(https?:\/\/(www\.?)?)?/
 const SEPARATORS = /[-\(]/
 
+let mutationObserverConfig = {
+  childList: true
+}
+let mutationObserver = new MutationObserver(function (mutationRecords) {
+  mutationRecords.forEach((mutationRecord) => {
+    if (typeof mutationRecord.addedNodes === "object") {
+      let searchResults = mutationRecord.target.querySelectorAll(".srg .g")
+      searchResults = [].slice.call(searchResults, 0, 10)
+      if (searchResults.length) {
+        let shoppingItem = searchResults.find((elem) => {
+          let domain = elem.querySelector('cite').innerText.replace(DOMAIN_PREFIX, "")
+          return POTENTIAL_VENDORS.find((vendor) => { return domain.startsWith(vendor) })
+        })
+
+        if (shoppingItem) {
+          let title = shoppingItem.querySelector('.r a').innerText
+          title = title.split(/[-\(]/, 1)[0].trim()
+          console.log("Title", title)
+        }
+      }
+    }
+  })
+})
 
 $(function() {
-  let searchTerm = ""
-
-  $("form[role=search]").on("submit", function () {
-    searchTerm = $(this).find("input[name=q]").val()
-    searchUpdated = true
-  })
-
-  $(document).on("DOMSubtreeModified", "#search", function () {
-    let currentSearchTerm = $("form[role=search] input[name=q]").val()
-    let searchResults = $(this).find('.srg .g')
-    if (searchTerm != currentSearchTerm && searchResults.length) {
-      let topSearchResults = searchResults.slice(0,10).toArray()
-      let shoppingItem = topSearchResults.find((elem) => {
-        let domain = $(elem).find('cite').text().replace(DOMAIN_PREFIX, "")
-        return POTENTIAL_VENDORS.find((vendor) => { return domain.startsWith(vendor) })
-      })
-
-      if (shoppingItem) {
-        let title = $(shoppingItem).find('.r a').text()
-        title = title.split(/[-\(]/, 1)[0].trim()
-        console.log("Title", title)
-      }
-      searchTerm = currentSearchTerm
+  $("form[role=search] input[name=q]").on("keydown", function (e) {
+    if (e.which === 13) {
+      mutationObserver.observe(document.querySelector('#search'), mutationObserverConfig)
     }
   })
 })
